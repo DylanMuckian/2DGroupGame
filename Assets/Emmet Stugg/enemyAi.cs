e87432752d041b6e49b842f185f9968ea0e70573
ObiLadd2 <McToal-E@ulster.ac.uk>
2024-03-12 14:55:35 +0000

    12/03/2024
    enemy AI . enemy health. player health.

--- Assets/Emmet Stugg/enemyAi.cs
+++ Assets/Emmet Stugg/enemyAi.cs
@@ -1,18 +1,71 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 
-public class enemyAi : MonoBehaviour
+public class aiMove : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public float speed;
+    public float checkRadius;
+    public float attackRadius;
+
+    public bool facingRight = true;
+    public bool LastfacingRight = true;
+
+    public LayerMask WhatisPlayer;
+
+    private Transform target;
+    private Rigidbody2D rb;
+    //private Animator anim;
+    private Vector2 movement;
+    public Vector3 dir;
+
+    private bool isinChaseRange;
+    private bool isinAttackRange;
+
+    private void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
+        //anim = GetComponent<Animator>();
+        target = GameObject.FindWithTag("Player").transform;
+    }
+    private void Update()
+    {
+        //anim.SetBool("isRunning", isinChaseRange);
+
+        isinChaseRange = Physics2D.OverlapCircle(transform.position, checkRadius, WhatisPlayer);
+        isinAttackRange = Physics2D.OverlapCircle(transform.position, attackRadius, WhatisPlayer);
+
+        dir = target.position - transform.position;
+        float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+        movement = dir;
+
+        if (dir.x >= 0) facingRight = true;
+        else facingRight = false;
+
+        if (facingRight != LastfacingRight)
+        {
+
+            transform.localScale = new Vector3(transform.localScale.x * -1, transform.localScale.y, transform.localScale.z);
+        }
+
+        LastfacingRight = facingRight;
+    }
+    private void FixedUpdate()
+    {
+        if (isinChaseRange && !isinAttackRange)
+        {
+            MoveChracter(movement);
+        }
+        if (isinAttackRange)
+        {
+            rb.velocity = Vector2.zero;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void MoveChracter(Vector2 dir)
     {
-        
+        rb.MovePosition((Vector2)transform.position + (dir * speed * Time.deltaTime));
     }
+
 }
---
+++ Assets/Emmet Stugg/playerHealth.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+
+public class PlayerHealth : MonoBehaviour
+{
+
+    Collider2D col;
+    public int spawn;
+    public float health = 100;
+    public float maxHealth = 100;
+    public Image healthbar;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        maxHealth = health;
+        col = GetComponent<Collider2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log(health);
+        healthbar.fillAmount = Mathf.Clamp(health / maxHealth, 0.0f, 1.0f);
+        if (health > maxHealth) health = maxHealth;
+    }
+
+
+}
