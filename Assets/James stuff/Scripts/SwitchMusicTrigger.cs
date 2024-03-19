95221da370f7651c3a23c0020633d2188304300a
jamesbrolly <148452080+jamesbrolly@users.noreply.github.com>
2024-03-19 14:45:43 +0000

    19/03/2024
    first boss music

--- Assets/James stuff/Scripts/SwitchMusicTrigger.cs
+++ Assets/James stuff/Scripts/SwitchMusicTrigger.cs
@@ -4,15 +4,31 @@ using UnityEngine;
 
 public class SwitchMusicTrigger : MonoBehaviour
 {
+    public AudioClip newTrack;
+
+    private AudioManager theAM;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        theAM = FindObjectOfType<AudioManager>();
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
+
+    void OnTriggerEnter2D(Collider2D other) 
+    { 
+      if(other.tag == "Player")
+        {
+            if(newTrack != null)
+            theAM.ChangeBGM(newTrack);
+        }
+    
+    }
+
 }
