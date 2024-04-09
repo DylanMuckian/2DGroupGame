20c0059774166595de92a0271c00a93d5cca7826
caitward647 <ward-c56@ulster.ac.uk>
2024-04-09 14:35:09 +0100

    Rotation MiniGame
    rotation completed on all objects. Display when E is pressed.

---
+++ Assets/Caitlins stuf/MiniGame/Scripts/Test.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.PackageManager;
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+    void Update()
+    {
+
+        if (hitInfo.collider.gameObject.tag == "MirrorEnd")
+        //Laser hits final mirror
+        {
+            Destroy(door);
+            //destroy door
+            //destroy mini game
+        }
+
+    }
+}
